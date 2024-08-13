542f271a1f715b6bdd3b3584efbd50a140480a27
Vasilis <tomaras.vasi@outlook.com>
2024-08-13 15:41:44 +0200

    playwirght tests added

---
+++ tests/frontpage.spec.js
@@ -0,0 +1,15 @@
+const { test, describe, expect, beforeEach } = require('@playwright/test')
+
+describe('Pokedex', () => {
+    test('front page can be opened', async ({ page }) => {
+        await page.goto('/')
+        await expect(page.getByText('ivysaur')).toBeVisible()
+        await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+    })
+    test('pokemon page can be navigated to', async ({ page }) => {
+        await page.goto('/')
+        await page.getByText('bulbasaur').click()
+        await page.getByText('next').click()
+        await expect(page.getByText('ivysaur')).toBeVisible()
+    })
+})
