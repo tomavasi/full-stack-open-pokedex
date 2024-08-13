bd8d7ffe32218192aac19deae176ad5189953ceb
Vasilis <tomaras.vasi@outlook.com>
2024-08-13 12:59:54 +0200

    changes on the linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,47 @@
-module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
-    "jest/globals": true
-  },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended"
-  ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  module.exports = {
+    "env": {
+      "browser": true,
+      "es6": true,
+      "jest/globals": true
     },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
-  },
-  "plugins": [
-    "react", "jest"
-  ],
-  "rules": {
-    "indent": [
-      "error",
+    "extends": [
+      "eslint:recommended",
+      "plugin:react/recommended",
+    ],
+    "parserOptions": {
+      "ecmaFeatures": {
+        "jsx": true
+      },
+      "ecmaVersion": 2018,
+      "sourceType": "module"
+    },
+    "plugins": [
+      "react", "jest"
+    ],
+    "rules": {
+      "indent": [
+        "error",
         2
       ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "double",
-    ],
-    "semi": [
-      "error",
-      "never"
-    ],
-    "eqeqeq": "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": [
-      "error", "always"
-    ],
-    "arrow-spacing": [
-      "error", { "before": true, "after": true }
-    ],
-    "no-console": "error",
-    "react/prop-types": 0
+      "linebreak-style": [
+        "error",
+        "unix",
+        // "windows"
+      ],
+      "quotes": [1, "single"],
+      "semi": [
+        "error",
+        "never"
+      ],
+      "eqeqeq": "error",
+      "no-trailing-spaces": "error",
+      "object-curly-spacing": [
+        "error", "always"
+      ],
+      "arrow-spacing": [
+        "error", { "before": true, "after": true }
+      ],
+      "no-console": "error",
+      "react/prop-types": 0
+    }
   }
-}
